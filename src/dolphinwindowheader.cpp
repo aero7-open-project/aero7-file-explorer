54bd369ae9950182c3fec0948614dec81abac4a3
Albert Tomanek <electron826@gmail.com>
2026-06-22 00:35:31 +0200

    All textboxes in window header are the same size.

--- src/dolphinwindowheader.cpp
+++ src/dolphinwindowheader.cpp
@@ -7,6 +7,11 @@ DolphinWindowHeader::DolphinWindowHeader(QWidget *parent)
     ui->setupUi(this);
 
     ui->searchBar->addAction(ui->actSearchOpts, QLineEdit::TrailingPosition);
+
+    /* Force all three textboxes to be the same height */
+    ui->primaryNavHole->setContentsMargins(QMargins(0, 0, 0, 0));
+    ui->secondaryNavHole->setContentsMargins(QMargins(0, 0, 0, 0));
+    ui->searchBar->setMaximumHeight(26);
 }
 
 DolphinWindowHeader::~DolphinWindowHeader()
